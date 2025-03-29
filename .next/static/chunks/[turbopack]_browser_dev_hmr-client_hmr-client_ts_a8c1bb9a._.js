0baf425f0a85a92c23da06f8b011c4afd24678d8
Diogoacg <a101919@alunos.uminho.pt>
2025-03-29 18:02:40 +0000

    apagar pasta next inutil

--- .next/static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_a8c1bb9a._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_f241ffa9._.js", {
-
-"[turbopack]/browser/dev/hmr-client/hmr-client.ts [app-client] (ecmascript, async loader)": (() => {{
-
-throw new Error("An error occurred while generating the chunk item [turbopack]/browser/dev/hmr-client/hmr-client.ts [app-client] (ecmascript, async loader)\n\nCaused by:\n- Expected visited target node to be module\n\nDebug info:\n- An error occurred while generating the chunk item [turbopack]/browser/dev/hmr-client/hmr-client.ts [app-client] (ecmascript, async loader)\n- Execution of <AsyncLoaderChunkItem as EcmascriptChunkItem>::content failed\n- Execution of AsyncLoaderChunkItem::chunks_data failed\n- Execution of AsyncLoaderChunkItem::chunks failed\n- Execution of ModuleGraph::module_batches failed\n- Execution of ModuleGraph::chunk_group_info failed\n- Expected visited target node to be module");
-
-}}),
-}]);
