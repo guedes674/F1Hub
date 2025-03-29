0a1d8bf7bca1ea8582b7123ffe6b3a1feefb6c30
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2025-03-29 23:24:33 +0000

    add the evnts to the database

--- Backend/schema.sql
+++ Backend/schema.sql
@@ -11,15 +11,24 @@ CREATE TABLE Player (
     Podiums INT NOT NULL,
     Points INT NOT NULL,
     ChampionshipWins INT NOT NULL,
-    PlayerNum INT NOT NULL
+    PlayerNum INT NOT NULL,
+    IsActive BOOLEAN NOT NULL
 );
 
 -- Creating the Event table to store events associated with players
--- CREATE TABLE Event (
---     EventId INT AUTO_INCREMENT PRIMARY KEY,
---     PlayerId INT,
---     EventDescription TEXT NOT NULL,
---     EventDate DATETIME DEFAULT CURRENT_TIMESTAMP,
---     FOREIGN KEY (PlayerId) REFERENCES Player(PlayerId) ON DELETE CASCADE
--- );
+ CREATE TABLE Event (
+    EventName VARCHAR(200) NOT NULL,
+    StartDate DATETIME NOT NULL PRIMARY KEY,
+    EndDate DATETIME NOT NULL,
+    Country VARCHAR(100) NOT NULL,
+    Location VARCHAR(100) NOT NULL,
+    Winner VARCHAR(100) NOT NULL,
+    FastLap INT NOT NULL
+ );
 
+-- Junction table for many-to-many relationship
+CREATE TABLE PlayerEvent (
+    PlayerId VARCHAR(100),
+    EventId DATETIME,
+    PRIMARY KEY (PlayerId, EventId)
+);
